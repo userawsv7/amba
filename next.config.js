50650c17beb3f56d98ea3317b71091765d8195b5
root <root@DESKTOP-23IMB3J.localdomain>
2026-07-04 23:58:42 +0000

    chore: full reset to pure javascript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
+  eslint: { ignoreDuringBuilds: true },
+  output: 'standalone'
 };
 module.exports = nextConfig;
